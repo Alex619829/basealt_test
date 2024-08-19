9cee6c893ed787b9791c0738d9772fe6ca152bca
Alex619829 <alexgamz1119@gmail.com>
2024-08-19 15:54:46 +0300

    error fixes

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@ if test -d /usr/local/bin/basealt_script; then
     rm /usr/local/bin/basealt_script
 fi
 
+cpan JSON
 
 cp main.pl /usr/local/bin/basealt_script
 
 chmod +x /usr/local/bin/basealt_script
 
-echo "Success: 'Base' installed to your computer"
+echo "Success: 'basealt_script' installed to your computer"
